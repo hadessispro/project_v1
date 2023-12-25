664071262244d1bb2968715c3320f356f62a1614
hadessispro <thaibaoleo123@gmail.com>
2023-12-25 19:26:00 +0700

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+const express=  require('express');
+const {default:helmet} =  require('helmet');
+const compresion = require('compresion');
+const {engine} =  require('express-handlebars')
+//use middlewave
+
+app.use(helmet());
+app.use(compresion())
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
+
+
